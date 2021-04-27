ea2feee7bd96487dc33be70a58b16c84dead619e
agnieszka <agnieszka.gfkm@o2.pl>
2021-04-27 21:45:10 +0200

    change controller and home.html to show total cases as of today

--- src/main/java/com/agacorpo/coronaTracker/controllers/HomeController.java
+++ src/main/java/com/agacorpo/coronaTracker/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package com.agacorpo.coronaTracker.controllers;
 
+import com.agacorpo.coronaTracker.models.LocationStats;
 import com.agacorpo.coronaTracker.services.CoronaDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -14,7 +17,10 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model){
+        List<LocationStats> allStats = coronaDataService.getStats();
+        int sumTotalCases = allStats.stream().mapToInt(LocationStats::getLatestTotalCases).sum();
         model.addAttribute("locationStats", coronaDataService.getStats());
+        model.addAttribute("totalCases", sumTotalCases);
         return "home";
     }
 }
