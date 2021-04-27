db64fb26d2b8ed696c359f93d4a2184748a99584
agnieszka <agnieszka.gfkm@o2.pl>
2021-04-27 19:32:28 +0200

    edit corona data service

--- src/main/java/com/agacorpo/coronaTracker/services/CoronaDataService.java
+++ src/main/java/com/agacorpo/coronaTracker/services/CoronaDataService.java
@@ -1,5 +1,6 @@
 package com.agacorpo.coronaTracker.services;
 
+import com.agacorpo.coronaTracker.models.LocationStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,15 +13,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CoronaDataService {
     private static String CORONA_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    private List<LocationStats> stats = new ArrayList<>();
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchCoronaData() throws IOException, InterruptedException {
-
+        List<LocationStats> newStats = new ArrayList<>();
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(CORONA_DATA_URL))
@@ -29,8 +33,14 @@ public class CoronaDataService {
         StringReader csvBodyReader = new StringReader(httpResponse.body());
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
         for (CSVRecord record : records) {
-            String state = record.get("Province/State");
-            System.out.println(state);
+            LocationStats locationStat = new LocationStats();
+            locationStat.setState(record.get("Province/State"));
+            locationStat.setCountry(record.get("Country/Region"));
+            //latest data:
+            locationStat.setState(record.get(record.size()-1));
+            System.out.println(locationStat);
+            newStats.add(locationStat);
         }
+        this.stats = newStats;
     }
 }
