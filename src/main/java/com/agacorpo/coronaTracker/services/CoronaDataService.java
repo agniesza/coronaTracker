e2c9b2bb1709a426b4c24e94e58f2193d87d9068
agnieszka <agnieszka.gfkm@o2.pl>
2021-04-27 19:11:30 +0200

    add Commons CSV to parse the header names

--- src/main/java/com/agacorpo/coronaTracker/services/CoronaDataService.java
+++ src/main/java/com/agacorpo/coronaTracker/services/CoronaDataService.java
@@ -1,9 +1,13 @@
 package com.agacorpo.coronaTracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -14,6 +18,7 @@ public class CoronaDataService {
     private static String CORONA_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchCoronaData() throws IOException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
@@ -21,6 +26,11 @@ public class CoronaDataService {
                 .uri(URI.create(CORONA_DATA_URL))
                 .build();
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            System.out.println(state);
+        }
     }
 }
